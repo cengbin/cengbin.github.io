d2181b527a25e2ab31f977687bf88140dcba4f04
bin.zeng <yy282371715@sina.cn>
2024-04-13 00:00:33 +0800

    build

---
+++ dist/static/frontend_40 同源策略与跨域_README.md.a280bfa4.js
@@ -0,0 +1,29 @@
+import{_ as a,o as s,c as e,Q as n}from"./chunks/framework.0f4e1e9e.js";const _=JSON.parse('{"title":"同源策略与跨域","description":"","frontmatter":{},"headers":[],"relativePath":"frontend/40 同源策略与跨域/README.md","filePath":"frontend/40 同源策略与跨域/README.md"}'),l={name:"frontend/40 同源策略与跨域/README.md"},p=n(`<h1 id="同源策略与跨域" tabindex="-1">同源策略与跨域 <a class="header-anchor" href="#同源策略与跨域" aria-label="Permalink to &quot;同源策略与跨域&quot;">​</a></h1><h2 id="_1、简介" tabindex="-1">1、简介 <a class="header-anchor" href="#_1、简介" aria-label="Permalink to &quot;1、简介&quot;">​</a></h2><h3 id="_1-1-什么是同源策略" tabindex="-1">1.1 什么是同源策略 <a class="header-anchor" href="#_1-1-什么是同源策略" aria-label="Permalink to &quot;1.1 什么是同源策略&quot;">​</a></h3><h2 id="_2、解决方案" tabindex="-1">2、解决方案 <a class="header-anchor" href="#_2、解决方案" aria-label="Permalink to &quot;2、解决方案&quot;">​</a></h2><h3 id="前端解决" tabindex="-1">前端解决： <a class="header-anchor" href="#前端解决" aria-label="Permalink to &quot;前端解决：&quot;">​</a></h3><p>通过没有同源策略限制的<code>&lt;script/&gt;</code>、<code>&lt;img/&gt;</code>等等标签发起一个请求</p><h4 id="jsonp" tabindex="-1">JSONP <a class="header-anchor" href="#jsonp" aria-label="Permalink to &quot;JSONP&quot;">​</a></h4><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">function jsonp(req) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">    var script = document.createElement(&#39;script&#39;);</span></span>
+<span class="line"><span style="color:#e1e4e8;">    var url = req.url + &#39;?callback=&#39; + req.callback.name;</span></span>
+<span class="line"><span style="color:#e1e4e8;">    script.src = url;</span></span>
+<span class="line"><span style="color:#e1e4e8;">    document.getElementsByTagName(&#39;head&#39;)[0].appendChild(script);</span></span>
+<span class="line"><span style="color:#e1e4e8;">}</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">function hello(res){</span></span>
+<span class="line"><span style="color:#e1e4e8;">    alert(&#39;hello &#39; + res.data);</span></span>
+<span class="line"><span style="color:#e1e4e8;">}</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">jsonp({</span></span>
+<span class="line"><span style="color:#e1e4e8;">    url : &#39;&#39;,</span></span>
+<span class="line"><span style="color:#e1e4e8;">    callback : hello </span></span>
+<span class="line"><span style="color:#e1e4e8;">});</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">function jsonp(req) {</span></span>
+<span class="line"><span style="color:#24292e;">    var script = document.createElement(&#39;script&#39;);</span></span>
+<span class="line"><span style="color:#24292e;">    var url = req.url + &#39;?callback=&#39; + req.callback.name;</span></span>
+<span class="line"><span style="color:#24292e;">    script.src = url;</span></span>
+<span class="line"><span style="color:#24292e;">    document.getElementsByTagName(&#39;head&#39;)[0].appendChild(script);</span></span>
+<span class="line"><span style="color:#24292e;">}</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">function hello(res){</span></span>
+<span class="line"><span style="color:#24292e;">    alert(&#39;hello &#39; + res.data);</span></span>
+<span class="line"><span style="color:#24292e;">}</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">jsonp({</span></span>
+<span class="line"><span style="color:#24292e;">    url : &#39;&#39;,</span></span>
+<span class="line"><span style="color:#24292e;">    callback : hello </span></span>
+<span class="line"><span style="color:#24292e;">});</span></span></code></pre></div><h3 id="后端解决" tabindex="-1">后端解决： <a class="header-anchor" href="#后端解决" aria-label="Permalink to &quot;后端解决：&quot;">​</a></h3><p>CORS</p><h3 id="服务器解决" tabindex="-1">服务器解决： <a class="header-anchor" href="#服务器解决" aria-label="Permalink to &quot;服务器解决：&quot;">​</a></h3><p>通过代理解决（Webpack、Vite、Nginx）</p>`,12),o=[p];function c(t,r,i,d,h,u){return s(),e("div",null,o)}const m=a(l,[["render",c]]);export{_ as __pageData,m as default};
---
+++ dist/static/frontend_98 笔记_CRUD 页面编写总结.md.05b7d712.js
@@ -0,0 +1,123 @@
+import{_ as s,o as n,c as a,Q as e}from"./chunks/framework.0f4e1e9e.js";const q=JSON.parse('{"title":"写CRUD页面总结","description":"","frontmatter":{},"headers":[],"relativePath":"frontend/98 笔记/CRUD 页面编写总结.md","filePath":"frontend/98 笔记/CRUD 页面编写总结.md"}'),l={name:"frontend/98 笔记/CRUD 页面编写总结.md"},t=e(`<h1 id="写crud页面总结" tabindex="-1">写CRUD页面总结 <a class="header-anchor" href="#写crud页面总结" aria-label="Permalink to &quot;写CRUD页面总结&quot;">​</a></h1><h2 id="场景一-数据-新增、编辑" tabindex="-1">场景一：数据 新增、编辑 <a class="header-anchor" href="#场景一-数据-新增、编辑" aria-label="Permalink to &quot;场景一：数据 新增、编辑&quot;">​</a></h2><h3 id="优化之前" tabindex="-1">优化之前 <a class="header-anchor" href="#优化之前" aria-label="Permalink to &quot;优化之前&quot;">​</a></h3><div class="language-page.vue vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">page.vue</span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">&lt;template&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	&lt;page&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">		&lt;button&gt;新增活动&lt;/button&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">		&lt;button&gt;新增服务器&lt;/button&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">		</span></span>
+<span class="line"><span style="color:#e1e4e8;">		&lt;el-dialog v-if=&quot;addActivityVisible&quot; ref=&#39;add-activity&#39;&gt;...&lt;/el-dialog&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">		&lt;el-dialog v-if=&quot;addServiceVisible&quot; ref=&#39;add-service&#39;&gt;...&lt;/el-dialog&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	&lt;page&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">&lt;/template&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">export default {</span></span>
+<span class="line"><span style="color:#e1e4e8;">	data(){</span></span>
+<span class="line"><span style="color:#e1e4e8;">		return {</span></span>
+<span class="line"><span style="color:#e1e4e8;">			addActivityVisible:false,</span></span>
+<span class="line"><span style="color:#e1e4e8;">			addServiceVisible:false,</span></span>
+<span class="line"><span style="color:#e1e4e8;">		}</span></span>
+<span class="line"><span style="color:#e1e4e8;">	}</span></span>
+<span class="line"><span style="color:#e1e4e8;">	onClickAddActivity(){</span></span>
+<span class="line"><span style="color:#e1e4e8;">		this.addActivityVisible=true;</span></span>
+<span class="line"><span style="color:#e1e4e8;">		...</span></span>
+<span class="line"><span style="color:#e1e4e8;">	}</span></span>
+<span class="line"><span style="color:#e1e4e8;">}</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">&lt;template&gt;</span></span>
+<span class="line"><span style="color:#24292e;">	&lt;page&gt;</span></span>
+<span class="line"><span style="color:#24292e;">		&lt;button&gt;新增活动&lt;/button&gt;</span></span>
+<span class="line"><span style="color:#24292e;">		&lt;button&gt;新增服务器&lt;/button&gt;</span></span>
+<span class="line"><span style="color:#24292e;">		</span></span>
+<span class="line"><span style="color:#24292e;">		&lt;el-dialog v-if=&quot;addActivityVisible&quot; ref=&#39;add-activity&#39;&gt;...&lt;/el-dialog&gt;</span></span>
+<span class="line"><span style="color:#24292e;">		&lt;el-dialog v-if=&quot;addServiceVisible&quot; ref=&#39;add-service&#39;&gt;...&lt;/el-dialog&gt;</span></span>
+<span class="line"><span style="color:#24292e;">	&lt;page&gt;</span></span>
+<span class="line"><span style="color:#24292e;">&lt;/template&gt;</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">export default {</span></span>
+<span class="line"><span style="color:#24292e;">	data(){</span></span>
+<span class="line"><span style="color:#24292e;">		return {</span></span>
+<span class="line"><span style="color:#24292e;">			addActivityVisible:false,</span></span>
+<span class="line"><span style="color:#24292e;">			addServiceVisible:false,</span></span>
+<span class="line"><span style="color:#24292e;">		}</span></span>
+<span class="line"><span style="color:#24292e;">	}</span></span>
+<span class="line"><span style="color:#24292e;">	onClickAddActivity(){</span></span>
+<span class="line"><span style="color:#24292e;">		this.addActivityVisible=true;</span></span>
+<span class="line"><span style="color:#24292e;">		...</span></span>
+<span class="line"><span style="color:#24292e;">	}</span></span>
+<span class="line"><span style="color:#24292e;">}</span></span></code></pre></div><h3 id="优化之后" tabindex="-1">优化之后 <a class="header-anchor" href="#优化之后" aria-label="Permalink to &quot;优化之后&quot;">​</a></h3><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;">&lt;template&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	&lt;page&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">		&lt;button&gt;新增&lt;/button&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	</span></span>
+<span class="line"><span style="color:#e1e4e8;">		&lt;activity ref=&#39;activity&#39;&gt;&lt;/activity&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	&lt;page&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">&lt;/template&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">export default {</span></span>
+<span class="line"><span style="color:#e1e4e8;">	onClickAddActivity(){</span></span>
+<span class="line"><span style="color:#e1e4e8;">		this.$refs[&#39;activity&#39;].show(type,data);</span></span>
+<span class="line"><span style="color:#e1e4e8;">	}</span></span>
+<span class="line"><span style="color:#e1e4e8;">}</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;">&lt;template&gt;</span></span>
+<span class="line"><span style="color:#24292e;">	&lt;page&gt;</span></span>
+<span class="line"><span style="color:#24292e;">		&lt;button&gt;新增&lt;/button&gt;</span></span>
+<span class="line"><span style="color:#24292e;">	</span></span>
+<span class="line"><span style="color:#24292e;">		&lt;activity ref=&#39;activity&#39;&gt;&lt;/activity&gt;</span></span>
+<span class="line"><span style="color:#24292e;">	&lt;page&gt;</span></span>
+<span class="line"><span style="color:#24292e;">&lt;/template&gt;</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">export default {</span></span>
+<span class="line"><span style="color:#24292e;">	onClickAddActivity(){</span></span>
+<span class="line"><span style="color:#24292e;">		this.$refs[&#39;activity&#39;].show(type,data);</span></span>
+<span class="line"><span style="color:#24292e;">	}</span></span>
+<span class="line"><span style="color:#24292e;">}</span></span></code></pre></div><h2 id="场景二-列表-增加、删除" tabindex="-1">场景二：列表 增加、删除 <a class="header-anchor" href="#场景二-列表-增加、删除" aria-label="Permalink to &quot;场景二：列表 增加、删除&quot;">​</a></h2><div class="language- vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark vp-code-dark"><code><span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">&lt;div v-for=&quot;(reward,index) in info.rewards&quot; :key=&quot;index&quot; class=&quot;condition-group&quot;&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	&lt;el-input v-model=&quot;reward.product_id&quot;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	          placeholder=&quot;请输入奖励ID&quot;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	          :disabled=&quot;server_opening&quot;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	          style=&quot;width:160px;margin-right: 8px;&quot;/&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	&lt;el-input v-model=&quot;reward.amount&quot; placeholder=&quot;请输入数值&quot;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	          :disabled=&quot;server_opening&quot;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	          style=&quot;width:160px;margin-right: 8px;&quot;/&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	&lt;el-button v-if=&quot;info.rewards.length&gt;1 &amp;&amp; !server_opening&quot;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	           class=&quot;button-custom&quot;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	           icon=&quot;el-icon-minus2&quot;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	           @click=&quot;onClickReward(&#39;delete&#39;,index)&quot;/&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	&lt;el-button v-if=&quot;index===info.rewards.length-1 &amp;&amp; !server_opening&quot;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	           icon=&quot;el-icon-plus2&quot;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	           class=&quot;button-custom&quot;</span></span>
+<span class="line"><span style="color:#e1e4e8;">	           @click=&quot;onClickReward(&#39;add&#39;,index)&quot;/&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;">&lt;/div&gt;</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">onClickReward(type, index) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">	if (type === &#39;add&#39;) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">		this.info.rewards.push({</span></span>
+<span class="line"><span style="color:#e1e4e8;">			&quot;product_id&quot;: null,</span></span>
+<span class="line"><span style="color:#e1e4e8;">			&quot;amount&quot;: null,</span></span>
+<span class="line"><span style="color:#e1e4e8;">		})</span></span>
+<span class="line"><span style="color:#e1e4e8;">	} else if (type === &#39;delete&#39;) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">		this.info.rewards.splice(index, 1)</span></span>
+<span class="line"><span style="color:#e1e4e8;">	}</span></span>
+<span class="line"><span style="color:#e1e4e8;">},</span></span></code></pre><pre class="shiki github-light vp-code-light"><code><span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">&lt;div v-for=&quot;(reward,index) in info.rewards&quot; :key=&quot;index&quot; class=&quot;condition-group&quot;&gt;</span></span>
+<span class="line"><span style="color:#24292e;">	&lt;el-input v-model=&quot;reward.product_id&quot;</span></span>
+<span class="line"><span style="color:#24292e;">	          placeholder=&quot;请输入奖励ID&quot;</span></span>
+<span class="line"><span style="color:#24292e;">	          :disabled=&quot;server_opening&quot;</span></span>
+<span class="line"><span style="color:#24292e;">	          style=&quot;width:160px;margin-right: 8px;&quot;/&gt;</span></span>
+<span class="line"><span style="color:#24292e;">	&lt;el-input v-model=&quot;reward.amount&quot; placeholder=&quot;请输入数值&quot;</span></span>
+<span class="line"><span style="color:#24292e;">	          :disabled=&quot;server_opening&quot;</span></span>
+<span class="line"><span style="color:#24292e;">	          style=&quot;width:160px;margin-right: 8px;&quot;/&gt;</span></span>
+<span class="line"><span style="color:#24292e;">	&lt;el-button v-if=&quot;info.rewards.length&gt;1 &amp;&amp; !server_opening&quot;</span></span>
+<span class="line"><span style="color:#24292e;">	           class=&quot;button-custom&quot;</span></span>
+<span class="line"><span style="color:#24292e;">	           icon=&quot;el-icon-minus2&quot;</span></span>
+<span class="line"><span style="color:#24292e;">	           @click=&quot;onClickReward(&#39;delete&#39;,index)&quot;/&gt;</span></span>
+<span class="line"><span style="color:#24292e;">	&lt;el-button v-if=&quot;index===info.rewards.length-1 &amp;&amp; !server_opening&quot;</span></span>
+<span class="line"><span style="color:#24292e;">	           icon=&quot;el-icon-plus2&quot;</span></span>
+<span class="line"><span style="color:#24292e;">	           class=&quot;button-custom&quot;</span></span>
+<span class="line"><span style="color:#24292e;">	           @click=&quot;onClickReward(&#39;add&#39;,index)&quot;/&gt;</span></span>
+<span class="line"><span style="color:#24292e;">&lt;/div&gt;</span></span>
+<span class="line"><span style="color:#24292e;"></span></span>
+<span class="line"><span style="color:#24292e;">onClickReward(type, index) {</span></span>
+<span class="line"><span style="color:#24292e;">	if (type === &#39;add&#39;) {</span></span>
+<span class="line"><span style="color:#24292e;">		this.info.rewards.push({</span></span>
+<span class="line"><span style="color:#24292e;">			&quot;product_id&quot;: null,</span></span>
+<span class="line"><span style="color:#24292e;">			&quot;amount&quot;: null,</span></span>
+<span class="line"><span style="color:#24292e;">		})</span></span>
+<span class="line"><span style="color:#24292e;">	} else if (type === &#39;delete&#39;) {</span></span>
+<span class="line"><span style="color:#24292e;">		this.info.rewards.splice(index, 1)</span></span>
+<span class="line"><span style="color:#24292e;">	}</span></span>
+<span class="line"><span style="color:#24292e;">},</span></span></code></pre></div><h3 id="场景三-表单提交" tabindex="-1">场景三：表单提交 <a class="header-anchor" href="#场景三-表单提交" aria-label="Permalink to &quot;场景三：表单提交&quot;">​</a></h3><h5 id="前端表单校验设计" tabindex="-1">前端表单校验设计： <a class="header-anchor" href="#前端表单校验设计" aria-label="Permalink to &quot;前端表单校验设计：&quot;">​</a></h5><p>职责：前端做非空、录入格式等等基本格式校验。服务端对业务逻辑的正确性进行校验。</p><p>理由：考虑到如果有业务逻辑调整只需要服务端一端修改校验逻辑即可，前端不需要做业务逻辑校验调整。</p>`,12),p=[t];function o(c,i,r,d,u,y){return n(),a("div",null,p)}const h=s(l,[["render",o]]);export{q as __pageData,h as default};
